92e922bd966612e1cc6e202006bf862cbea10d88
zoidberg <zoidberg.ufo@gmail.com>
2024-08-19 21:20:34 +0300

    Turn off console command to check telegram updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command(TelegramBotCommand::class)->everyMinute();
+//        $schedule->command(TelegramBotCommand::class)->everyMinute();
     }
 
     /**
